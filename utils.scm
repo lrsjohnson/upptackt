49f342000dfbceeb625cbde97f95048047e4e7db
Lars Johnson <larsj@mit.edu>
2015-03-07 21:22:12 -0500

    Good enuf from scmutils

---
+++ utils.scm
@@ -0,0 +1,32 @@
+;;; close-enuf? floating point comparison from scmutils
+;;; Origin: Gerald Jay Sussman
+
+(define *machine-epsilon*
+  (let loop ((e 1.0))
+    (if (= 1.0 (+ e 1.0))
+        (* 2 e)
+        (loop (/ e 2)))))
+
+(define *sqrt-machine-epsilon*
+  (sqrt *machine-epsilon*))
+
+#|
+ (define (close-enuf? h1 h2 tolerance)
+   (<= (magnitude (- h1 h2))
+       (* .5 (max tolerance *machine-epsilon*)
+          (+ (magnitude h1) (magnitude h2) 2.0))))
+|#
+
+(define (close-enuf? h1 h2 #!optional tolerance scale)
+  (if (default-object? tolerance)
+      (set! tolerance *machine-epsilon*))
+  (if (default-object? scale)
+      (set! scale 1.0))
+  (<= (magnitude (- h1 h2))
+      (* tolerance
+         (+ (* 0.5
+               (+ (magnitude h1) (magnitude h2)))
+            scale))))
+
+(define (assert boolean error-message)
+  (if (not boolean) (error error-message)))
