e606c365f40fdfb2013f495e00aa5e1446404413
Lars Johnson <larsj@mit.edu>
2015-04-07 19:13:44 -0400

    Adding distances from point to line.

--- figure/measurements.scm
+++ figure/measurements.scm
@@ -6,6 +6,28 @@
            (square (- (point-y p1)
                       (point-y p2))))))
 
+;;; Sign of distance is positive if the point is to the left of
+;;; the line direction and negative if to the right.
+(define (signed-distance-to-line point line)
+  (let ((p1 (line-p1 line))
+        (p2 (line-p2 line)))
+    (let ((x0 (point-x point))
+          (y0 (point-y point))
+          (x1 (point-x p1))
+          (y1 (point-y p1))
+          (x2 (point-x p2))
+          (y2 (point-y p2)))
+      (/ (+ (- (* x0 (- y2 y1)))
+            (* y0 (- x2 x1))
+            (- (* x2 y1))
+            (* y2 x1))
+         (* 1.0
+            (sqrt (+ (square (- y2 y1))
+                     (square (- x2 x1)))))))))
+
+(define (distance-to-line point line)
+  (abs (signed-distance-to-line point line)))
+
 (define (angle-measure a)
   (let* ((d1 (angle-arm-1 a))
          (d2 (angle-arm-2 a)))
