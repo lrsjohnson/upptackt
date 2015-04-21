d127c924b17b537635fb5208d8ec7542c6c36656
Lars Johnson <larsj@mit.edu>
2015-04-20 23:11:23 -0400

    Dependencies for polygon references

--- figure/polygon.scm
+++ figure/polygon.scm
@@ -5,24 +5,50 @@
   (%polygon n-points points)
   polygon?
   (n-points polygon-n-points)
-  (points polygon-points))
+  (points %polygon-points))
 
 (define (polygon-from-points . points)
   (let ((n-points (length points)))
     (%polygon n-points points)))
 
+;;; Internal reference for polygon points
 (define (polygon-point-ref polygon i)
   (if (>= i (polygon-n-points polygon))
-      (error "polygon-point-ref not in range"))
-  (list-ref (polygon-points polygon) i))
-;;; TODO: Dependencies of points obtained from polygon.
+      (error "polygon point index not in range"))
+  (list-ref (%polygon-points polygon) i))
+
+(define (polygon-points polygon)
+  (map (lambda (i) (polygon-point polygon i))
+       (iota (polygon-n-points polygon))))
+
+;;; External polygon points including dependencies
+(define (polygon-point polygon i)
+  ;;: TODO: Handle situations where polygon isn't terminal dependency
+  (with-dependency ;;-if-unknown
+   `(polygon-point ,i ,(element-dependency polygon))
+   (polygon-point-ref polygon i)))
+
+;;; i and j are indices of adjacent points
+(define (polygon-segment polygon i j)
+  (let ((n-points (polygon-n-points polygon)))
+   (cond
+    ((not (or (= i (modulo (+ j 1) n-points))
+              (= j (modulo (+ i 1) n-points))))
+     (error "polygon-segment must be called with adjacent indices"))
+    ((or (>= i n-points)
+         (>= j n-points))
+     (error "polygon-segment point index out of range"))
+    (else
+     (let* ((p1 (polygon-point-ref polygon i))
+            (p2 (polygon-point-ref polygon j))
+            (segment (make-segment p1 p2)))
+       ;;: TODO: Handle situations where polygon isn't terminal dependency
+       (with-dependency
+        `(polygon-segment ,i ,j ,(element-dependency polygon))
+        segment))))))
 
 (define (polygon-segments polygon)
   (let ((n-points (polygon-n-points polygon)))
-    (map (lambda (pair)
-           (make-segment (polygon-point-ref polygon (car pair))
-                         (polygon-point-ref polygon (cadr pair))))
-         (zip
-          (iota n-points)
-          (map (lambda (i) (modulo (+ i 1) n-points))
-               (iota n-points))))))
+    (map (lambda (i)
+           (polygon-segment polygon i (modulo (+ i 1) n-points)))
+         (iota n-points))))
