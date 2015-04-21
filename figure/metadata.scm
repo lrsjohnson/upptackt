e95fbd2c3a23ff556ce58ad8705e00a739639733
Lars Johnson <larsj@mit.edu>
2015-04-20 23:10:38 -0400

    Initial infrastructure for dependencies

--- figure/metadata.scm
+++ figure/metadata.scm
@@ -1,11 +1,38 @@
 ;;; Assign name
 (define (set-element-name! element name)
-  (eq-put! element 'name name))
+  (eq-put! element 'name name)
+  element)
 
 (define (element-name element)
   (or (eq-get element 'name)
       '*unnamed*))
 
+;;; Dependencies:
+(define (set-dependency! element dependency)
+  (eq-put! element 'dependency dependency))
+
+(define (with-dependency dependency element)
+  (set-dependency! element dependency)
+  element)
+
+(define (with-dependency-if-unknown dependency element)
+  (if (dependency-unknown? element)
+      (with-dependency dependency element)
+      element))
+
+(define *unknown-dependency* (list '*unknown-dependency*))
+(define (unknown-dependency? x)
+  (eq? x *unknown-dependency*))
+
+(define (dependency-unknown? element)
+  (unknown-dependency? (element-dependency element)))
+
+(define (element-dependency element)
+  (or (eq-get element 'dependency)
+      *unknown-dependency*))
+
+
+;;; Initial attempt at dependencies:
 (define (mark-known-equal seg1 seg2)
   (if (not (and (segment? seg1)
                 (segment? seg2)))
