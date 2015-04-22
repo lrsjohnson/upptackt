99c5f60a143af15d7dc25cb923b830368bd1a2ea
Lars Johnson <larsj@mit.edu>
2015-04-21 21:46:37 -0400

    Archive old component ideas.

--- manipulate/component-old-ideas.scm
+++ manipulate/component-old-ideas.scm
@@ -19,15 +19,20 @@
 
 (define (merge-component))
 ;;; (1) Find two point-connections in separate components with the same name
-;;; (2) Translate to align these point connections
-;;; (3) Mark the second component as extending from the first at that merged point-connection via some arbitrary angle
-;;; (4) For each new pair of points, check if their distance has been established in any other diagram, add "constraints"
-;;;       or try to rotate the new angle to satisfy such
 
-;;; Rotate + extend one length? [extend length for desired distance, then rotate to meet desired point]
+;;; (2) Translate to align these point connections
+
+;;; (3) Mark the second component as extending from the first at that merged
+;;; point-connection via some arbitrary angle
+
+;;; (4) For each new pair of points, check if their distance has been
+;;; established in any other diagram, add "constraints"
+;;; or try to rotate the new angle to satisfy such
+
+;;; Rotate + extend one length? [extend length for desired distance, then rotate
+;;; to meet desired point]
 
 ;;; (4) Satisfy other point connections in the diagram:
 ;;;     - Check distances between point connections in the components
-
 
 ;;; Multiple angles with equality???
