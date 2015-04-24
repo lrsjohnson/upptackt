eb5715daf1b4bc6e8fe5dac1452f737945400b3c
Lars Johnson <larsj@mit.edu>
2015-04-24 12:21:28 -0400

    Basic region info in propagator cells.

--- manipulate/region-info.scm
+++ manipulate/region-info.scm
@@ -87,13 +87,18 @@
                      (lp (cdr points-1)
                          points-2
                          point-intersections)))))))
-    ;; TODO: Process contradiction object better.
-    (apply m:make-point-set result)))
+    (if (> (length result) 0)
+        (apply m:make-point-set result)
+        ;;; TODO: Determine error from pairwise distances
+        (m:make-region-contradiction 0))))
 
 (define m:intersect-regions (make-generic-operation 2 'm:intersect-regions))
 
 (defhandler m:intersect-regions
   m:intersect-point-sets m:point-set? m:point-set?)
+
+(defhandler m:intersect-regions (lambda (a b) a) m:region-contradiction? any?)
+(defhandler m:intersect-regions (lambda (a b) b) any? m:region-contradiction?)
 
 ;;;;;;;;;;;;;;;;;;; Interface to Propagator System ;;;;;;;;;;;;;;;;;;;
 
@@ -106,8 +111,25 @@
 (define m:region-equivalent?
   (make-generic-operation 2 'm:region-equivalent? (lambda (a b) #f)))
 
-;(defhandler equivalent? m:region-equivalent? m:region? m:region?)
+(defhandler equivalent? m:region-equivalent? m:region? m:region?)
 
-;(defhandler merge m:intersect-regions m:region? m:region?)
+(defhandler merge m:intersect-regions m:region? m:region?)
 
-;(defhandler contradictory? m:region-contradiction? m:region?)
+(defhandler contradictory? m:region-contradiction? m:region?)
+
+#|
+ (let ((a (make-point 0 0))
+       (b (make-point 1 0))
+       (c (make-point 0 1))
+       (d (make-point 1 1)))
+     (let-cells (cell)
+       (add-content cell
+                    (make-tms
+                     (contingent (m:make-point-set a b c)
+                                 '(a))))
+       (add-content cell
+                    (make-tms
+                     (contingent (m:make-point-set a d)
+                                 '(a))))
+       (pp (tms-query (content cell)))))
+|#
